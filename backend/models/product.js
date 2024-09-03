a817e0cb0e05a51ed0c767d0b0907e8496dabef2
PreethiGopal119 <106738216+RajanRajRtr@users.noreply.github.com>
2024-09-03 18:03:32 +0530

    TO be  backend inital setup

---
+++ backend/models/product.js
@@ -0,0 +1,60 @@
+import mongoose from "mongoose";
+// import { v4 as uuidv4 } from 'uuid';
+import AutoIncrementFactory from "mongoose-sequence";
+
+const { Schema } = mongoose;
+
+// Initialize the auto-increment factory
+const AutoIncrement = AutoIncrementFactory(mongoose);
+
+const productSchema = new Schema({
+    title: { type: String, required: true },
+    price: { type: String, required: true },
+    discountPrice: { type: String },
+    collection_: { type: String },
+    color: { type: String},
+    category: { type: String},
+    images: { type: [String] },
+    description: { type: String, required: true },
+    LongDesc: { type: String },
+    feature: { type: Boolean},
+    rating: { type: Number },
+    review: { type: Number},
+    offer: { type: String },
+    quantity_stock: { type: String, required: true },
+    productId: { type: Number, unique: true }, // Auto-incrementing field
+	specification:[
+	
+			{
+			  product_Details: {
+				feel:{ type: String },
+				cover_Type: { type: String },
+				cover_Material: { type: String },
+				matress_Type:
+				{ type: String },
+				Usability:
+				{ type: String },
+			  },
+			  product_Dimension: {
+				thickness:
+				{ type: String },
+				dimensions:
+				{ type: String },
+			  },
+			  product_Policies: {
+				Warranty: { type: String },
+				Shipping: { type: String },
+				available_Offers: { type: Number },
+				trial: { type: String },
+			  },
+			},
+		  ],
+	
+});
+
+
+
+// Apply the auto-increment plugin to the product schema
+productSchema.plugin(AutoIncrement, { inc_field: 'productId' });
+
+export const Product = mongoose.model("Product", productSchema);
---
+++ backend/routes/product.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+
+import { getAllProducts, getOneProduct,createProduct } from "../controllers/product.js";
+
+const router = Router();
+
+router.get("/:productId", getOneProduct);
+router.get("/", getAllProducts);
+// router.post("/create", createProduct);
+router.post('/create', upload.array('images', 5), createProduct);
+
+
+export default router;
