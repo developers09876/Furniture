36d75b9b5c6d93829abfef1004365d3c89b0dddd
Ganesh90448 <ganeshmurugan90448@gmail.com>
2024-09-14 12:31:10 +0530

    fixed backend user reigeter api

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,6 +1,10 @@
 import { Router } from "express";
 
-import { getAllProducts, getOneProduct,createProduct } from "../controllers/product.js";
+import {
+  getAllProducts,
+  getOneProduct,
+  createProduct,
+} from "../controllers/product.js";
 
 const router = Router();
 
@@ -8,6 +12,5 @@ router.get("/:productId", getOneProduct);
 router.get("/", getAllProducts);
 router.post("/create", createProduct);
 // router.post('/create', upload.array('images', 5), createProduct);
-
 
 export default router;
