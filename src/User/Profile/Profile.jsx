5544bef258f1a233e455bed74523a7ea7df27988
Subitha879 <subithasubi489@gmail.com>
2024-09-19 15:46:37 +0530

    Profile  form fixed bug

--- src/User/Profile/Profile.jsx
+++ src/User/Profile/Profile.jsx
@@ -1,75 +1,190 @@
-import { Divider, Table } from "antd";
-import React from "react";
-import { Col, Row } from "react-bootstrap";
-import { MdDelete, MdEdit } from "react-icons/md";
+// import { Divider, Table } from "antd";
+// import React from "react";
+// import { Col, Row } from "react-bootstrap";
+// import { MdDelete, MdEdit } from "react-icons/md";
+// import styled from "styled-components";
+
+// const StyledProfile = styled.div`
+//   margin: 20px;
+//   margin-left: 250px;
+//   margin-right: auto;
+//   width: 100%;
+// `;
+
+// const columns = [
+//   {
+//     title: "Sno",
+//     dataIndex: "key",
+//   },
+//   {
+//     title: "Name",
+//     dataIndex: "name",
+//   },
+//   {
+//     title: "Description",
+//     dataIndex: "description",
+//   },
+//   {
+//     title: "Action",
+//     key: "Action",
+//     render: (_, record) => (
+//       <div>
+//         <Row>
+//           <Col md={3}>
+//             <a>
+//               <MdEdit style={{ fontSize: "20px" }} />
+//             </a>
+//           </Col>
+//           <Col md={3}>
+//             <a>
+//               <MdDelete style={{ fontSize: "20px" }} />
+//             </a>
+//           </Col>
+//         </Row>
+//       </div>
+//     ),
+//   },
+// ];
+
+// const data = [
+//   {
+//     key: "1",
+//     name: "user1",
+//     description: "Lorem ipsum dolor sit amet.",
+//   },
+//   {
+//     key: "2",
+//     name: "user2",
+//     description: "Lorem ipsum dolor sit amet consectetur.",
+//   },
+//   {
+//     key: "3",
+//     name: "user3",
+//     description: "Lorem ipsum dolo amet consectetur.",
+//   },
+// ];
+// function Profile() {
+//   return (
+//     <StyledProfile>
+//       <div>
+//         <Divider style={{ fontSize: "30px" }}>Profile</Divider>
+//         <Table columns={columns} dataSource={data} size="middle" />
+//       </div>
+//     </StyledProfile>
+//   );
+// }
+
+// export default Profile;
+
+
+
+
+import { Divider, Form, Input, Button } from "antd";
+import React, { useState } from "react";
+import { Row, Col } from "react-bootstrap"; 
 import styled from "styled-components";
 
 const StyledProfile = styled.div`
   margin: 20px;
-  margin-left: 250px;
+  margin-left: 50px;
   margin-right: auto;
   width: 100%;
 `;
 
-const columns = [
+const initialData = [
   {
-    title: "Sno",
-    dataIndex: "key",
-  },
-  {
-    title: "Name",
-    dataIndex: "name",
-  },
-  {
-    title: "Description",
-    dataIndex: "description",
-  },
-  {
-    title: "Action",
-    key: "Action",
-    render: (_, record) => (
-      <div>
-        <Row>
-          <Col md={3}>
-            <a>
-              <MdEdit style={{ fontSize: "20px" }} />
-            </a>
-          </Col>
-          <Col md={3}>
-            <a>
-              <MdDelete style={{ fontSize: "20px" }} />
-            </a>
-          </Col>
-        </Row>
-      </div>
-    ),
+    key: "1",
+    name: "name",
+    email: "name@example.com",
+    phonenumber: "1234567890",
+    password: "password",
   },
 ];
 
-const data = [
-  {
-    key: "1",
-    name: "user1",
-    description: "Lorem ipsum dolor sit amet.",
-  },
-  {
-    key: "2",
-    name: "user2",
-    description: "Lorem ipsum dolor sit amet consectetur.",
-  },
-  {
-    key: "3",
-    name: "user3",
-    description: "Lorem ipsum dolo amet consectetur.",
-  },
-];
 function Profile() {
+  const [form] = Form.useForm();
+  const [data, setData] = useState(initialData);
+
+  const handleUpdate = (values, key) => {
+    const updatedData = data.map((item) =>
+      item.key === key ? { ...item, ...values } : item
+    );
+    setData(updatedData);
+  };
+
+  const handleCancel = () => {
+    form.resetFields(); 
+  };
+
   return (
     <StyledProfile>
-      <div>
-        <Divider style={{ fontSize: "30px" }}>Profile</Divider>
-        <Table columns={columns} dataSource={data} size="middle" />
-      </div>
+      <Divider style={{ fontSize: "30px" }}>Profile</Divider>
+      {data.map((user) => (
+        <Form style={{marginLeft:'250px',width:'70%'}}
+          form={form}
+          key={user.key}
+          initialValues={user}
+          onFinish={(values) => handleUpdate(values, user.key)}
+          layout="vertical"
+        >
+          <Row>
+            <Col xs={12} sm={6}>
+              <Form.Item
+                label="Name"
+                name="name"
+                rules={[{ required: true, message: "Please enter your name!" }]}
+              >
+                <Input />
+              </Form.Item>
+            </Col>
+
+       
+            <Col xs={12} sm={6}>
+              <Form.Item label="Email" name="email">
+                <Input disabled />
+              </Form.Item>
+            </Col>
+          </Row>
+
+          <Row>
+           
+            <Col xs={12} sm={6}>
+              <Form.Item
+                label="Phonenumber"
+                name="phonenumber"
+                rules={[{ required: true, message: "Please enter your phone number!" }]}
+              >
+                <Input />
+              </Form.Item>
+            </Col>
+
+            
+            <Col xs={12} sm={6}>
+              <Form.Item
+                label="Password"
+                name="password"
+                rules={[{ required: true, message: "Please enter your password!" }]}
+              >
+                <Input.Password />
+              </Form.Item>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col xs={12}>
+              <Button type="primary" htmlType="submit">
+                Update
+              </Button>
+              <Button
+                style={{ marginLeft: "10px" }}
+                onClick={handleCancel}
+              >
+                Cancel
+              </Button>
+            </Col>
+          </Row>
+        </Form>
+      ))}
     </StyledProfile>
   );
 }
--- src/dashboard/users/Users.jsx
+++ src/dashboard/users/Users.jsx
@@ -1,6 +1,294 @@
+// import { Divider, Table, Modal, Button, Form, Input, Select } from "antd";
+// import { Col, Row } from "react-bootstrap";
+// import { useContext, useEffect,useState } from "react";
+// import { MdEdit, MdDelete } from "react-icons/md";
+// import { DashboardContext } from "../../context/DashboardContext";
+// import Swal from "sweetalert2";
+// import styled from "styled-components";
+// import AddUser from "../users/AddUser";
+// import axios from "axios";
+
+// const { confirm } = Modal;
+// const { Option } = Select;
+
+// const StyledUsers = styled.div`
+//   margin: 20px;
+//   margin-left: 250px;
+//   margin-right: auto;
+//   width: 100%;
+// `;
+
+// const StyledTh = styled.th`
+//   padding: 10px 25px !important;
+// `;
+
+// const StyledTd = styled.th`
+//   padding: 10px 25px !important;
+//   font-weight: 400;
+// `;
+
+// const initialData = [
+//   {
+//     key: "1",
+//     name: "Ganesh",
+//     email: "user1@gmail.com",
+//     phone: "1234567890",
+//     role: "Admin",
+//   },
+//   {
+//     key: "2",
+//     name: "Jim Green",
+//     email: "user2@gmail.com",
+//     phone: "2345678901",
+//     role: "User",
+//   },
+//   {
+//     key: "3",
+//     name: "Joe Black",
+//     email: "user3@gmail.com",
+//     phone: "3456789012",
+//     role: "Manager",
+//   },
+// ];
+
+// const Users = () => {
+//   const { addUser } = useContext(DashboardContext);
+//   const [Adduser, setAdduser] = useState(false);
+//   const [EditUser, setEditUser] = useState(false);
+//   const [selectedUser, setSelectedUser] = useState(null);
+//   const [form] = Form.useForm();
+//   const [data, setData] = useState(initialData); // State to manage table data
+
+ 
+//   const [users, setUsers] = useState([]);
+  
+
+//   useEffect(() => {
+//     const fetchUsersData = async () => {
+//       try {
+//         const res = await axios.get("http://localhost:5000/User/get");
+//         setUsers(res.data);
+//       } catch (error) {
+//         console.error("Error fetching category:", error);
+//       }
+//     };
+
+//     fetchUsersData();
+//   }, []);
+
+
+//   const handleEdit = (record) => {
+//     setSelectedUser(record);
+//     form.setFieldsValue(record);
+//     setEditUser(true);
+//   };
+
+//   const handleEditSubmit = (values) => {
+//     const updatedData = data.map((user) =>
+//       user.key === selectedUser.key ? { ...user, ...values } : user
+//     );
+//     setData(updatedData); // Update the user details in the data
+
+//     Swal.fire({
+//       icon: "success",
+//       title: "Updated!",
+//       text: `User ${values.name} has been updated.`,
+//     });
+//     setEditUser(false);
+//   };
+   
+
+
+//   const deleteRecordFromAPI = async (id) => {
+//     console.log("subi", id);
+
+//     try {
+//       const response = await axios.delete(
+//         `http://localhost:5000/user/delete/${id}`
+//       );
+
+//       if (response.status === 200) {
+//         const newData = data.filter((item) => item.key !== key);
+//         setData(newData);
+
+//         Swal.fire({
+//           icon: "success",
+//           title: "Deleted!",
+//           text: `User has been deleted successfully.`,
+//         });
+//       }
+//     } catch (error) {
+//       console.error("Error deleting record:", error);
+//       Swal.fire({
+//         icon: "error",
+//         title: "Error!",
+//         text: "There was an error deleting the user. Please try again.",
+//       });
+//     }
+//   };
+
+//   const handleDelete = (record, data, setData) => {
+//     console.log("firstrecord", record);
+//     confirm({
+//       title: "Are you sure you want to delete this user?",
+//       icon: <MdDelete style={{ fontSize: "20px", color: "red" }} />,
+//       content: `Name: ${record.name}`,
+//       okText: "Yes",
+//       okType: "danger",
+//       cancelText: "No",
+//       onOk() {
+//         deleteRecordFromAPI(record._id); // Call the API to delete the record
+//       },
+//       onCancel() {
+//         console.log("Deletion cancelled");
+//       },
+//     });
+//   }
+//   const columns = [
+//     {
+//       title: "Sno",
+//       render: (i, record, index) => (
+//         <div>
+//           <p>
+//             {index + 1}
+//           </p>
+//         </div>
+//       )
+//     },
+//     {
+//       title: "Name",
+//       dataIndex: "username",
+//     },
+//     {
+//       title: "Email",
+//       dataIndex: "email",
+//     },
+//     {
+//       title: "Phone",
+//       dataIndex: "phoneNumber",
+//     },
+//     {
+//       title: "Role",
+//       dataIndex: "role",
+//     },
+//     {
+//       title: "Action",
+//       key: "action",
+//       render: (_, record) => (
+//         <Row>
+//           <Col md={3}>
+//             <a onClick={() => handleEdit(record)}>
+//               <MdEdit style={{ fontSize: "20px", cursor: "pointer" }} />
+              
+//             </a>
+//           </Col>
+//           <Col md={3}>
+           
+//           <MdDelete
+//             style={{ fontSize: "20px", cursor: "pointer", color: "red" }}
+//             onClick={() => handleDelete(record, data, setData)}
+//           />
+//           </Col>
+//         </Row>
+//       ),
+//     },
+//   ];
+
+//   //
+
+//   return (
+//     <StyledUsers>
+//        {/* <Button style={{marginLeft:'90%'}} type="primary" onClick={() => setAdduser(true)}>
+//         Add User
+//       </Button> */}
+//       <Divider style={{ fontSize: "30px" }}>Customers</Divider>
+//       <Table columns={columns} dataSource={users} size="middle" />
+      
+
+//       <Modal
+//         centered
+//         open={Adduser}
+//         onCancel={() => setAdduser(false)}
+//         footer={null}
+//       >
+//         <AddUser setAdduser={setAdduser} />
+//       </Modal>
+
+//       <Modal
+//         title="Edit User"
+//         centered
+//         open={EditUser}
+//         onCancel={() => setEditUser(false)}
+//         footer={[
+//           <Button key="cancel" onClick={() => setEditUser(false)}>
+//             Cancel
+//           </Button>,
+//           <Button
+//             key="submit"
+//             type="primary"
+//             onClick={() => {
+//               form.submit();
+//             }}
+//           >
+//             Save
+//           </Button>,
+//         ]}
+//       >
+//         <Form
+//           form={form}
+//           layout="vertical"
+//           onFinish={handleEditSubmit}
+//           initialValues={selectedUser}
+//         >
+//           <Form.Item
+//             name="name"
+//             label="Name"
+//             rules={[{ required: true, message: "Please enter the name" }]}
+//           >
+//             <Input placeholder="Enter name" />
+//           </Form.Item>
+//           <Form.Item
+//             name="email"
+//             label="Email"
+//             rules={[{ required: true, message: "Please enter the email" }]}
+//           >
+//             <Input placeholder="Enter email" />
+//           </Form.Item>
+//           <Form.Item
+//             name="phone"
+//             label="Phone"
+//             rules={[
+//               { required: true, message: "Please enter the phone number" },
+//             ]}
+//           >
+//             <Input placeholder="Enter phone number" />
+//           </Form.Item>
+//           <Form.Item
+//             name="role"
+//             label="Role"
+//             rules={[{ required: true, message: "Please select a role" }]}
+//           >
+//             <Select placeholder="Select a role">
+//               <Option value="Admin">Admin</Option>
+//               <Option value="Manager">Manager</Option>
+//               <Option value="User">User</Option>
+//             </Select>
+//           </Form.Item>
+//         </Form>
+//       </Modal>
+//     </StyledUsers>
+//   );
+// };
+
+// export default Users;
+
+
+
+
+
 import { Divider, Table, Modal, Button, Form, Input, Select } from "antd";
 import { Col, Row } from "react-bootstrap";
-import { useContext, useEffect,useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { MdEdit, MdDelete } from "react-icons/md";
 import { DashboardContext } from "../../context/DashboardContext";
 import Swal from "sweetalert2";
@@ -27,41 +315,13 @@ const StyledTd = styled.th`
   font-weight: 400;
 `;
 
-const initialData = [
-  {
-    key: "1",
-    name: "Ganesh",
-    email: "user1@gmail.com",
-    phone: "1234567890",
-    role: "Admin",
-  },
-  {
-    key: "2",
-    name: "Jim Green",
-    email: "user2@gmail.com",
-    phone: "2345678901",
-    role: "User",
-  },
-  {
-    key: "3",
-    name: "Joe Black",
-    email: "user3@gmail.com",
-    phone: "3456789012",
-    role: "Manager",
-  },
-];
-
 const Users = () => {
   const { addUser } = useContext(DashboardContext);
   const [Adduser, setAdduser] = useState(false);
   const [EditUser, setEditUser] = useState(false);
   const [selectedUser, setSelectedUser] = useState(null);
   const [form] = Form.useForm();
-  const [data, setData] = useState(initialData); // State to manage table data
-
- 
   const [users, setUsers] = useState([]);
-  
 
   useEffect(() => {
     const fetchUsersData = async () => {
@@ -69,7 +329,7 @@ const Users = () => {
         const res = await axios.get("http://localhost:5000/User/get");
         setUsers(res.data);
       } catch (error) {
-        console.error("Error fetching category:", error);
+        console.error("Error fetching users:", error);
       }
     };
 
@@ -77,48 +337,25 @@ const Users = () => {
   }, []);
 
 
-  const handleEdit = (record) => {
-    setSelectedUser(record);
-    form.setFieldsValue(record);
-    setEditUser(true);
-  };
-
-  const handleEditSubmit = (values) => {
-    const updatedData = data.map((user) =>
-      user.key === selectedUser.key ? { ...user, ...values } : user
-    );
-    setData(updatedData); // Update the user details in the data
-
-    Swal.fire({
-      icon: "success",
-      title: "Updated!",
-      text: `User ${values.name} has been updated.`,
-    });
-    setEditUser(false);
-  };
-   
-
-
   const deleteRecordFromAPI = async (id) => {
-    console.log("subi", id);
-
     try {
       const response = await axios.delete(
         `http://localhost:5000/user/delete/${id}`
       );
 
       if (response.status === 200) {
-        const newData = data.filter((item) => item.key !== key);
-        setData(newData);
+        // Remove the user from the local state (filtered by _id from the API)
+        const updatedUsers = users.filter((user) => user._id !== id);
+        setUsers(updatedUsers);
 
         Swal.fire({
           icon: "success",
           title: "Deleted!",
-          text: `User has been deleted successfully.`,
+          text: "User has been deleted successfully.",
         });
       }
     } catch (error) {
-      console.error("Error deleting record:", error);
+      console.error("Error deleting user:", error);
       Swal.fire({
         icon: "error",
         title: "Error!",
@@ -127,33 +364,81 @@ const Users = () => {
     }
   };
 
-  const handleDelete = (record, data, setData) => {
-    console.log("firstrecord", record);
+
+  const handleDelete = (record) => {
     confirm({
-      title: "Are you sure you want to delete this user?",
+      title: `Are you sure you want to delete ${record.username}?`,
       icon: <MdDelete style={{ fontSize: "20px", color: "red" }} />,
-      content: `Name: ${record.name}`,
+      content: `Name: ${record.username}`, // Show the name of the user dynamically
       okText: "Yes",
       okType: "danger",
       cancelText: "No",
       onOk() {
-        deleteRecordFromAPI(record._id); // Call the API to delete the record
+        deleteRecordFromAPI(record._id); // Use the _id to call the API for deletion
       },
       onCancel() {
         console.log("Deletion cancelled");
       },
     });
-  }
+  };
+
+  const handleEdit = (record) => {
+    setSelectedUser(record);
+    form.setFieldsValue(record);
+    setEditUser(true);
+  };
+
+  const handleEditSubmit = (record) => {
+    confirm({
+      title: `Are you sure you want to update ${record.username}?`,
+      icon: <MdEdit style={{ fontSize: "20px", color: "red" }} />,
+      content: `Name: ${record.username}`, // Show the name of the user dynamically
+      okText: "Yes",
+      okType: "danger",
+      cancelText: "No",
+      onOk() {
+        editRecordFromAPI(selectedUser._id, record); // Pass the updated values and ID
+      },
+      onCancel() {
+        console.log("Edit cancelled");
+      },
+    });
+  };
+  
+  const editRecordFromAPI = async (id, record) => {
+    try {
+      await axios.post(`http://localhost:5000/user/update/${id}`, record)
+        .then((res) => {
+          Swal.fire({
+            icon: "success",
+            title: "Updated!",
+            text: `User has been updated successfully.`,
+          });
+  
+          // Update the user details in the local state
+          const updatedUsers = users.map((user) =>
+            user._id === id ? { ...user, ...record } : user
+          );
+          setUsers(updatedUsers);
+  
+          // Close the edit modal
+          setEditUser(false);
+        });
+    } catch (error) {
+      console.error("Error updating user:", error);
+      Swal.fire({
+        icon: "error",
+        title: "Error!",
+        text: "There was an error updating the user. Please try again.",
+      });
+    }
+  };
+  
+
   const columns = [
     {
       title: "Sno",
-      render: (i, record, index) => (
-        <div>
-          <p>
-            {index + 1}
-          </p>
-        </div>
-      )
+      render: (i, record, index) => <p>{index + 1}</p>,
     },
     {
       title: "Name",
@@ -177,33 +462,26 @@ const Users = () => {
       render: (_, record) => (
         <Row>
           <Col md={3}>
-            <a onClick={() => handleEdit(record)}>
-              <MdEdit style={{ fontSize: "20px", cursor: "pointer" }} />
-              
-            </a>
+            <MdEdit
+              style={{ fontSize: "20px", cursor: "pointer" }}
+              onClick={() => handleEdit(record)}
+            />
           </Col>
           <Col md={3}>
-           
-          <MdDelete
-            style={{ fontSize: "20px", cursor: "pointer", color: "red" }}
-            onClick={() => handleDelete(record, data, setData)}
-          />
+            <MdDelete
+              style={{ fontSize: "20px", cursor: "pointer", color: "red" }}
+              onClick={() => handleDelete(record)}
+            />
           </Col>
         </Row>
       ),
     },
   ];
 
-  //
-
   return (
     <StyledUsers>
-       {/* <Button style={{marginLeft:'90%'}} type="primary" onClick={() => setAdduser(true)}>
-        Add User
-      </Button> */}
       <Divider style={{ fontSize: "30px" }}>Customers</Divider>
       <Table columns={columns} dataSource={users} size="middle" />
-      
 
       <Modal
         centered
@@ -226,9 +504,7 @@ const Users = () => {
           <Button
             key="submit"
             type="primary"
-            onClick={() => {
-              form.submit();
-            }}
+            onClick={() => form.submit()}
           >
             Save
           </Button>,
@@ -241,7 +517,7 @@ const Users = () => {
           initialValues={selectedUser}
         >
           <Form.Item
-            name="name"
+            name="username"
             label="Name"
             rules={[{ required: true, message: "Please enter the name" }]}
           >
@@ -255,7 +531,7 @@ const Users = () => {
             <Input placeholder="Enter email" />
           </Form.Item>
           <Form.Item
-            name="phone"
+            name="phoneNumber"
             label="Phone"
             rules={[
               { required: true, message: "Please enter the phone number" },
@@ -263,7 +539,7 @@ const Users = () => {
           >
             <Input placeholder="Enter phone number" />
           </Form.Item>
-          <Form.Item
+          {/* <Form.Item
             name="role"
             label="Role"
             rules={[{ required: true, message: "Please select a role" }]}
@@ -273,7 +549,7 @@ const Users = () => {
               <Option value="Manager">Manager</Option>
               <Option value="User">User</Option>
             </Select>
-          </Form.Item>
+          </Form.Item> */}
         </Form>
       </Modal>
     </StyledUsers>
