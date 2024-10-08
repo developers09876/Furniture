aed64c0aa1c597b2757e5788035eb72d01117523
PreethiGopal119 <106738216+RajanRajRtr@users.noreply.github.com>
2024-10-08 17:22:15 +0530

    "Updated .env file, fixed catch block in category controller, refactored server.js, and modified App.jsx routes"

--- backend/controllers/category.js
+++ backend/controllers/category.js
@@ -4,8 +4,8 @@ export const getAllCategory = async (req, res) => {
   try {
     const allProduct = await Category.find();
     res.status(200).json(allProduct);
-  } catch {
-    res.status(500).json({ message: error.message });
+  } catch(e) {
+    res.status(500).json({ message: e.message });
   }
 };
 
--- backend/server.js
+++ backend/server.js
@@ -4,22 +4,19 @@ import cors from "cors";
 import morgan from "morgan";
 import mongoose from "mongoose";
 
+import userRouter from "./routes/user.js";
 import initProductRouter from "./routes/init.js";
 import featuredItemsRouter from "./routes/featuredItems.js";
 import productsRouter from "./routes/product.js";
 import basketRouter from "./routes/basket.js";
 import CategoryRouter from "./routes/category.js";
-import userRouter from "./routes/user.js";
-
 const app = express();
 const port = process.env.PORT || 5000;
 const dbUrl = process.env.DB_URL;
 // app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(morgan("dev"));
-app.use(cors({
-  origin:"http://13.233.115.17/"
-}));
+app.use(cors());
 app.use(express.json());
 // routes
 
