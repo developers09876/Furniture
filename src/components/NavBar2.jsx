909a17fa7acaf460a554f846f1fb0ada6901e134
Subitha879 <subithasubi489@gmail.com>
2024-10-19 14:19:32 +0530

    NavBar changes push

--- src/components/NavBar2.jsx
+++ src/components/NavBar2.jsx
@@ -4,17 +4,17 @@ import styled from 'styled-components';
 
 
 const TopOfferBar = styled.div`
-  background-color: var( --heading-clr) !important;
+  background-color: black;
   text-align: center;
-  color: white;
   font-size: 14px;
   height:40px;
   margin:0;
+  padding:10px;
   `;
 
 const StyledCarousel = styled(Carousel)`
   .slick-dots li button {
-    background: white;
+    // background: white;
   }
 `;
 
@@ -22,10 +22,14 @@ const TopBar = () => (
     <TopOfferBar>
         <StyledCarousel autoplay dots={false}>
             <div >
-                Use code <span style={{ color: 'white' }}>MEGAFEST</span> to Get up to 75% off + Additional 10% off with bank offers
+                <span style={{ color: 'white' }}>Use code <span style={{ color: 'red', fontWeight: 'bold' }}>MEGAFEST</span> to Get up to 75% off + Additional 10% off with bank offers</span>
             </div>
-            <div>Special Offer! Free Shipping on All Orders Above $500</div>
-            <div>92+ Restopedic Furniture Stores across India. <span style={{ color: 'white' }}>Come, Visit Us!</span></div>
+            <div>
+                <span style={{ color: 'white' }}>Special Offer! Free Shipping on All Orders Above $500</span>
+            </div>
+            <div>
+                <span style={{ color: 'white' }}> 92+ Restopedic Furniture Stores across India. </span> <span style={{ color: 'red', fontWeight: 'bold' }}>Come, Visit Us!</span>
+            </div>
         </StyledCarousel>
     </TopOfferBar>
 );
