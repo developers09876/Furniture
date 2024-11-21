8225e8c7d7e9bff0e71e80e2de06f24ad1f1b031
Subitha879 <subithasubi489@gmail.com>
2024-11-21 16:19:26 +0530

    Orderchanges push

--- src/User/UserOrders/UserOrders.jsx
+++ src/User/UserOrders/UserOrders.jsx
@@ -193,6 +193,7 @@ const UserOrders = () => {
         >
           <option value="all">All</option>
           <option value="pending">Pending </option>
+          <option value="Inprogress">In Progress</option>
           <option value="shipped">Shipped</option>
           <option value="delivered">Delivered</option>
           <option value="cancelled">Cancelled</option>
